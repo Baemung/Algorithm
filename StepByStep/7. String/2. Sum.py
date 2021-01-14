1fcf3a5690e776365712fcf6ee71f9d71610a60e
Mungyu Bae <anvo930524@gmail.com>
2021-01-14 18:54:13 +0900

    Delete StepByStep/7. String directory

--- StepByStep/7. String/2. Sum.py
+++
@@ -1,8 +0,0 @@
-num = int(input())
-code = input()
-sum = 0
-
-for i in range(num):
-    sum += int(code[i])
-
-print(sum)
--- StepByStep/7. String/4. Iterate String.py
+++
@@ -1,8 +0,0 @@
-testCase = int(input())
-
-for i in range(testCase):
-    a, b = input().split()
-    new_b = ''
-    for j in b:
-        new_b += j*int(a)
-    print(new_b)
