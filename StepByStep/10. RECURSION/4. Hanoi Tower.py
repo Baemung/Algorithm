44a7444433c673b1ce33907c99bad91b83310478
mung <anvo930524@gmail.com>
2021-01-21 19:31:31 +0900

    10

--- StepByStep/10. RECURSION/4. Hanoi Tower.py
+++ StepByStep/10. RECURSION/4. Hanoi Tower.py
@@ -1,14 +1,10 @@
 def hanoi(n, a, b, c):
     if(n == 0):
         return
-    hanoi(n-1,a,c,b)
-    print(a,c)
-    hanoi(n-1,b,a,c)
+    hanoi(n-1, a, c, b)
+    print(a, c)
+    hanoi(n-1, b, a, c)
 
-n, cnt, mv = int(input()), 0, 0
-while(cnt != n):
-    mv = 2*mv + 1
-    cnt += 1
-
-print(mv)
+n = int(input())
+print(2**n - 1)
 hanoi(n, 1, 2, 3)
