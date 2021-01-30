8ba745a14df0b13d714e1c84b59510316343207d
mung <anvo930524@gmail.com>
2021-01-31 04:26:54 +0900

    14

---
+++ StepByStep/14. DYNAMIC PROGRAMMING/8. Make 1.py
@@ -0,0 +1,13 @@
+n = [0]*(int(input())+1)
+
+for i in range(2,len(n)):
+    if(i%3 == 0 and i%2 == 0):
+        n[i] = min(n[i-1],n[i//3],n[i//2])+1
+    if(i%3 == 0):
+        n[i] = min(n[i-1],n[i//3])+1
+    elif(i%2 == 0):
+        n[i] = min(n[i-1],n[i//2])+1
+    else:
+        n[i] = n[i-1]+1
+
+print(n[-1])
