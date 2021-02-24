bdf964511c0cc18433922b6607bb2266e339b216
mung <anvo930524@gmail.com>
2021-02-24 10:40:54 +0900

    4

--- StepByStep/4. WHILE/3. Add Cycle.py
+++ StepByStep/4. WHILE/3. Add Cycle.py
@@ -1,13 +1,10 @@
 num = int(input())
-copy = num
-cnt = 0
-
+copy, cnt = num, 0
 while True:
     new_num =copy%10
-    copy = (copy//10 + copy%10)%10
-    copy = new_num*10 + copy
+    copy = (copy//10+copy%10)%10
+    copy = new_num*10+copy
     cnt += 1
-    if (copy == num):
+    if copy == num:
         break
-
 print(cnt)
