1a40c03aecdafec28b62c93185683228d0248736
mung <anvo930524@gmail.com>
2021-02-24 10:30:19 +0900

    1

--- StepByStep/1. IO&OPERATOR/10. Remainder.py
+++ StepByStep/1. IO&OPERATOR/10. Remainder.py
@@ -3,5 +3,4 @@ test1A = (a+b)%c
 test1B = ((a%c)+(b%c))%c
 test2A = (a*b)%c
 test2B = ((a%c)*(b%c))%c
-
 print(test1A, test1B, test2A, test2B, sep="\n")
--- StepByStep/1. IO&OPERATOR/9. Basic Operation.py
+++ StepByStep/1. IO&OPERATOR/9. Basic Operation.py
@@ -4,5 +4,4 @@ sub = a-b
 mul = a*b
 div = a//b
 rem = a%b
-
 print(add, sub, mul, div, rem, sep="\n")
