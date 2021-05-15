fafbcb464f201200508c099e9ad7de4c701ba496
mung <anvo930524@gmail.com>
2021-05-15 21:33:30 +0900

    28

--- StepByStep/28. TREE/1. Find Parent Node.py
+++ StepByStep/28. TREE/1. Find Parent Node.py
@@ -1,22 +1,24 @@
 import sys
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 
-class Node:
-    def __init__(self, data, parent):
-        self.data = data
-        self.parent = parent
+def dfs(start, tree, parents):
+    for i in tree[start]: #연결된 노드 모두 탐색
+        if parents[i] == 0: #한번도 방문한 적이 없다면
+            parents[i] = start #부모노드 저장
+            dfs(i, tree, parents) #해당 노드의 자식노드 탐색
 
 n = int(input())
-tree = [list(map(int, input().split())) for _ in range(n-1)]
+tree = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
+    tree[b].append(a)
 
-root = Node(1, None)
-for i in tree:
-    for j in range(n):
-        if j in i:
-            if i.index(j) == 0:
-                child1 = Node(i[0], root)
-            else:
-                child1 = Node(i[1], root)
+#부모 저장 리스트
+parents = [0 for _ in range(n+1)]
 
+dfs(1, tree, parents)
 
-print(n, tree)
+for i in range(2, n+1):
+    print(parents[i])
