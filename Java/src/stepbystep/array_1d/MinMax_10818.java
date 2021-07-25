b79fcbe7c8168cf199c31be3092e9245d4c8b729
배문규 <anvo930524@gmail.com>
2021-07-25 23:10:50 +0900

    1d array java

--- Java/src/stepbystep/array_1d/MinMax_10818.java
+++ Java/src/stepbystep/array_1d/MinMax_10818.java
@@ -1,10 +1,22 @@
 package stepbystep.array_1d;
 
+import java.util.Scanner;
+
 public class MinMax_10818 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int _min = Integer.MAX_VALUE, _max = Integer.MIN_VALUE;
+		
+		for(int i = 0; i < n; i++) {
+			int temp = sc.nextInt();
+			if(temp >= _max) _max = temp;
+			if(temp <= _min) _min = temp;
+		}
+		
+		System.out.println(_min+" "+_max);
 	}
 
 }
