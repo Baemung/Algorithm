cb8004718d9e39b2e81d6433c1814a139498d190
배문규 <anvo930524@gmail.com>
2021-07-31 22:25:40 +0900

    0730

--- Java/src/study/date0730/BaekOJ11047.java
+++ Java/src/study/date0730/BaekOJ11047.java
@@ -1,22 +1,23 @@
 package study.date0730;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class BaekOJ11047 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = sc.nextInt();
-		int K = sc.nextInt();
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int K = Integer.parseInt(input[1]);
 		int cnt = 0;
-		
-		//sc.nextLine();
 		
 		int[] money = new int[N];
 		for(int i = N-1; i >= 0; i--) {
-			money[i] = sc.nextInt();
+			money[i] = Integer.parseInt(br.readLine());
 		}
 				
 		for(int m:money) {
