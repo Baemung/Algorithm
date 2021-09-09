795fdcb3094ec40d213703f2b6f28f87c9168535
배문규 <anvo930524@gmail.com>
2021-09-09 20:43:35 +0900

    rename hit1 get4

--- HIT1_GET4/src/SEOUL06_BAEMUNGYU.java
+++ HIT1_GET4/src/SEOUL06_BAEMUNGYU.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class DAEJEON02_JAVA {
+public class SEOUL06_BAEMUNGYU {
 
 	// 닉네임을 사용자에 맞게 변경해 주세요.
 	static final String NICKNAME = "SEOUL06_BAEMUNGYU";
